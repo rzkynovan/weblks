51df2d3e2db4c71fc4beb8df011ebef0bd76bb23
rzkynovan <rizkynovan21@gmail.com>
2022-03-21 20:40:36 +0700

    model,seed.middleware

--- database/migrations/2022_03_21_125230_add_foreignid_artikel.php
+++ database/migrations/2022_03_21_125230_add_foreignid_artikel.php
@@ -13,10 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('artikels', function (Blueprint $table) {
+            $table -> foreignId('tag_id')->constrained();
         });
     }
 
@@ -27,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::table('artikels', function (Blueprint $table) {
+            //
+        });
     }
 };
